9d3e96140c2f8605c08cf1ef6751d39715f4700f
wekilos <wekilyazgulyyew2000@gmail.com>
2021-09-10 16:58:13 +0500

    change limit 100b to 1000mb

--- servers.js
+++ servers.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 1111;
  
 
 app.use("/uploads",express.static("uploads"));
-app.use(bodyParser.json({limit: "100mb"}));
-app.use(bodyParser.urlencoded({limit: '100mb', extended: true,parameterLimit:50000}));
+app.use(bodyParser.json({limit: "1000mb"}));
+app.use(bodyParser.urlencoded({limit: '1000mb', extended: true,parameterLimit:50000}));
 app.use(
   cors({
     origin:"*"
